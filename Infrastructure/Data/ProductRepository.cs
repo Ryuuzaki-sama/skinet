66d500bf8a6c879693f6e7288c36fe4b469219ea
Hassan Belmekki <hassanbelmekki@gmail.com>
2022-01-12 12:58:12 +0100

    First commit rearrange code

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext context;
+        public ProductRepository(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+            return await context.ProductBrands.ToListAsync();
+        }
+
+        public async Task<Product> GetProductByUdAsync(int id)
+        {
+            return await context.Products.FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await context.Products.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            return await context.ProductTypes.ToListAsync();
+        }
+    }
+}
