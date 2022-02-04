189fd01d9196f49bf625d3ce120d207b2752f8db
Hassan Belmekki <hassanbelmekki@gmail.com>
2022-02-04 22:11:11 +0100

    Pagination part2

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -38,9 +38,9 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts([FromQuery]ProductSpecParams productParams) 
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
             var products = await productsRepo.ListAsync(spec);
             return Ok( mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
